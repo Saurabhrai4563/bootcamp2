1f9402470ead91d90639bf62d7e9db1b54116ea6
Saurabh Shekhar Rai <122366063+Saurabhrai4563@users.noreply.github.com>
2026-05-27 22:42:22 +0530

    diff of last and first
    we gave to find diff between last and first element in given array

---
+++ Array/Test1/diffLastAndFirst.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> arr{1, 2, 3, 4, 5, 8};
+
+    cout << "Diff last - first :" << arr[arr.size() - 1] - arr[0] << endl;
+    // code
+    return 0;
+}
